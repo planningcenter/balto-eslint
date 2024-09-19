c2eaebd173390cb17efa35fce4d4941ccd5b3b5c
Nick Sheck <sheck@users.noreply.github.com>
2024-09-19 13:20:00 -0700

    Undo simulated changes

--- test/v8/existing.js
+++ test/v8/existing.js
@@ -1,5 +1,3 @@
 function hello(this_should_be_ignored_because_its_an_existing_violation) {
-   console.log("hello world")
-  const a = 0
-  a = 1
+  console.log("hello world")
 }
--- test/v8/new.js
+++
@@ -1,9 +0,0 @@
-function addOne(i) {
-    if (i != NaN) {
-        return i ++
-    } else {
-      return
-    }
-};
-
-const not_camel_case = true
