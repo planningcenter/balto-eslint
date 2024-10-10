3affcca70f19fe0874c7fd94c2de94c5961bf53d
Nick Sheck <sheck@users.noreply.github.com>
2024-10-10 15:31:19 -0700

    Automated: Simulate changes

---
+++ test/v8/new.js
@@ -0,0 +1,9 @@
+function addOne(i) {
+    if (i != NaN) {
+        return i ++
+    } else {
+      return
+    }
+};
+
+const not_camel_case = true
--- test/v9/existing.js
+++ test/v9/existing.js
@@ -1,3 +1,5 @@
 function hello(this_should_be_ignored_because_its_an_existing_violation) {
-  console.log("hello world")
+   console.log("hello world")
+  const a = 0
+  a = 1
 }
